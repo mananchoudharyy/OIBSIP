58695032b088c4bfd8baa36a0b3d5b49a4856640
mananchoudharyy <168512964+mananchoudharyy@users.noreply.github.com>
2024-06-02 16:24:21 +0530

    Add files via upload

---
+++ task - 4/QuestionAnswer.java
@@ -0,0 +1,27 @@
+package com.example.quizapplication;
+
+public class QuestionAnswer {
+
+    public static String[] question ={
+            " What is the capital of France?",
+            "Which one is not programming language",
+            " What is the largest planet in our solar system?",
+            "What is the chemical symbol for water?"
+
+    };
+
+    public static String[][] choices = {
+            {"Paris","London","Berlin","Tokyo"},
+            {"Java","Kotlin","Notepad","Python"},
+            {"Earth","Jupiter","Saturn","Mars"},
+            {"H₂O","CO₂","O₂"," NaCl"}
+
+    };
+
+    public static String[] correctAnswer = {
+            "Paris",
+            "Notepad",
+            "Jupiter",
+            "H₂O"
+    };
+}
